e738226efaba0bab0eb04985bc3a9331e13d1ff2
berliantino <berliantino@gmail.com>
2015-03-09 13:29:42 +0700

    setting database

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'berliantino';
+$db['default']['password'] = 'silence1234';
+$db['default']['database'] = 'makanapaya';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
